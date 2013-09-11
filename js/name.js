d669247536d763645fdb2c0b1d766ea8b392fca3
Michele Guido <micheleguido@gmail.com>
2013-09-11 10:16:16 +0200

    i will be so eexcited when this finally works. first commit

---
+++ js/name.js
@@ -0,0 +1,60 @@
+var phoneticAlphabet = ["alpha", "bravo", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliett", "kilo", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "zulu"]
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+var name = "michele"
+var splitName = name.split("")
+
+var phoneticName
+
+console.log(splitName)
+
+/*function test() {
+    var phoneticName = splitName.replace(/m/g, "b");
+}
+
+console.log(phoneticName)
+
+console.log(phoneticAlphabet[0]) */
+
+function logArrayElements(element, index, array) {
+	console.log("a[" + index + "] = " + element);
+}
+
+splitName.forEach ()
+
+if splitName
+
+
+
+
+
+// Match one d followed by one or more b's followed by one d
+// Remember matched b's and the following d
+// Ignore case
+
+//var myRe = /d(b+)(d)/i;
+//var myArray = myRe.exec("cdbBdbsbz");
+
+
+//console.log(splitName.splice(alphabet, phoneticAlphabet))
+
+//console.log(splitName.indexOf("i"))
+
+
+
+
+//console.log(phoneticAlphabet.indexOf())
+
+
+/*[1,2,3].map(function(i) { return i*2; } )
+
+splitName.map(
+	function() {
+		return
+	}
+
+	)*/
+
+
+
